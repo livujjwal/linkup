dccab20ccca5b819df1f5a558cb687c8068e9b36
livujjwal <ujjwalsingh2112@gamil.com>
2024-04-04 22:09:01 +0530

    user validation done

--- Utils/UserValidation.js
+++ Utils/UserValidation.js
@@ -1,10 +1,26 @@
+const emailRegex = (email) => {
+  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+};
+const passwordRegex = (password) => {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+    password
+  );
+};
 const userValidation = ({ name, email, username, password }) => {
   return new Promise((resolve, reject) => {
-    console.log(name, email, username, password);
     if (!name || !email || !username || !password)
       reject("All fields are required");
-    if (name.length < 4 || name.length > 50)
-      reject("Name should be in 4-50 character");
+    checkLength(name, "Name");
+    checkLength(username, "Username");
+    checkLength(password, "Password");
+    function checkLength(entry, entryTitle) {
+      console.log(entry, entryTitle);
+      if (entry.length < 4 || entry.length > 50)
+        reject(`${entryTitle} should be in 4-50 character`);
+    }
+    console.log(emailRegex(email));
+    if (!emailRegex(email)) reject("Please enter valid email");
+    // if (!passwordRegex(password)) reject("Please enter strong password");
     resolve();
   });
 };
